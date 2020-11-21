0e7e80c0e77615effc4d50cd1378bc985e51e80c
y724u <68385107+y724u@users.noreply.github.com>
2020-11-21 22:11:04 +0900

    JS修正

--- script.js
+++ script.js
@@ -1,36 +1,27 @@
 
-  $(function() {
-    // 1. 「全選択」する
-    $('#language-all').on('click', function() {
-      $("input[name='language']").prop('checked', this.checked);
-    });
-    // 2. 「全選択」以外のチェックボックスがクリックされたら、
-    $("input[name='language']").on('click', function() {
-      if ($('#check__item :checked').length == $('#check__item :input').length) {
-        // 全てのチェックボックスにチェックが入っていたら、「全選択」 = checked
-        $('#language-all').prop('checked', true);
-      } else {
-        // 1つでもチェックが入っていたら、「全選択」 = checked
-        $('#language-all').prop('checked', false);
-      }
-    });
+$(function () {
+  $('#js-language').on('click', function () {
+    $("input[name='language']").prop('checked', this.checked);
+  });
+  $("input[name='language']").on('click', function () {
+    if ($('#check__item :checked').length == $('#check__item :input').length) {
+      $('#js-language').prop('checked', true);
+    } else {
+      $('#js-language').prop('checked', false);
+    }
   });
 
-  $(function() {
-    // 1. 「全選択」する
-    $('#income-all').on('click', function() {
-      $("input[name='income']").prop('checked', this.checked);
-    });
-    // 2. 「全選択」以外のチェックボックスがクリックされたら、
-    $("input[name='income']").on('click', function() {
-      if ($('#check__item2 :checked').length == $('#check__item2 :input').length) {
-        // 全てのチェックボックスにチェックが入っていたら、「全選択」 = checked
-        $('#income-all').prop('checked', true);
-      } else {
-        // 1つでもチェックが入っていたら、「全選択」 = checked
-        $('#income-all').prop('checked', false);
-      }
-    });
+  $('#js-income').on('click', function () {
+    $("input[name='income']").prop('checked', this.checked);
   });
+  $("input[name='income']").on('click', function () {
+    if ($('#check__item2 :checked').length == $('#check__item2 :input').length) {
+      $('#js-income').prop('checked', true);
+    } else {
+      $('#js-income').prop('checked', false);
+    }
+  });
+});
+
 
 
