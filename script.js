9d826018f6b77af19deb3f81d637181294105a5a
y724u <68385107+y724u@users.noreply.github.com>
2020-11-21 21:01:09 +0900

    チェックボックスを作成

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
-$(function () {
+
   $(function() {
     // 1. 「全選択」する
-    $('#all').on('click', function() {
+    $('#language-all').on('click', function() {
       $("input[name='language']").prop('checked', this.checked);
     });
     // 2. 「全選択」以外のチェックボックスがクリックされたら、
     $("input[name='language']").on('click', function() {
       if ($('#check__item :checked').length == $('#check__item :input').length) {
         // 全てのチェックボックスにチェックが入っていたら、「全選択」 = checked
-        $('#all').prop('checked', true);
+        $('#language-all').prop('checked', true);
       } else {
         // 1つでもチェックが入っていたら、「全選択」 = checked
-        $('#all').prop('checked', false);
+        $('#language-all').prop('checked', false);
       }
     });
   });
 
   $(function() {
     // 1. 「全選択」する
-    $('#all-second').on('click', function() {
+    $('#income-all').on('click', function() {
       $("input[name='income']").prop('checked', this.checked);
     });
     // 2. 「全選択」以外のチェックボックスがクリックされたら、
     $("input[name='income']").on('click', function() {
       if ($('#check__item2 :checked').length == $('#check__item2 :input').length) {
         // 全てのチェックボックスにチェックが入っていたら、「全選択」 = checked
-        $('#all-second').prop('checked', true);
+        $('#income-all').prop('checked', true);
       } else {
         // 1つでもチェックが入っていたら、「全選択」 = checked
-        $('#all-second').prop('checked', false);
+        $('#income-all').prop('checked', false);
       }
     });
   });
-});
+
+
