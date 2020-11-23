7adf149f90dee6b74f8a8b2bc8a51c1ed3a982d7
y724u <68385107+y724u@users.noreply.github.com>
2020-11-23 21:27:01 +0900

    JS 全選択の記述を修正

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
-
 $(function () {
-  $('#js-language').on('click', function () {
-    $(".js-languageItem input").prop('checked', this.checked);
-  });
-  $(".js-languageItem").on('click', function () {
-    if ($('.js-languageItem :checked').length == $('.js-languageItem :input').length) {
-      $('#js-language').prop('checked', true);
-    } else {
-      $('#js-language').prop('checked', false);
-    }
-  });
-
-  $('#js-income').on('click', function () {
-    $(".js-checkItem input").prop('checked', this.checked);
-  });
-  $(".js-checkItem input").on('click', function () {
-    if ($('.js-checkItem :checked').length == $('.js-checkItem :input').length) {
-      $('#js-income').prop('checked', true);
-    } else {
-      $('#js-income').prop('checked', false);
-    }
+  $('.js-all').on('click', function () {
+    const target = $(this).data('target');
+    const checkItem = document.getElementById(target);
+    $($(checkItem).find('input')).prop('checked', this.checked);
   });
 });
+
+// $(function () {
+
+//   $(".js-languageItem").on('click', function () {
+//     if ($('.js-languageItem :checked').length == $('.js-languageItem :input').length) {
+//       $('#js-language').prop('checked', true);
+//     } else {
+//       $('#js-language').prop('checked', false);
+//     }
+//   });
+
+//   $(".js-checkItem input").on('click', function () {
+//     if ($('.js-checkItem :checked').length == $('.js-checkItem :input').length) {
+//       $('#js-income').prop('checked', true);
+//     } else {
+//       $('#js-income').prop('checked', false);
+//     }
+//   });
+// });
+
+// $('.js-modalOpen').on('click', function () {
+//   const target = $(this).data('target');
+//   const modal = document.getElementById(target);
+//   scrollPosition1 = $(window).scrollTop();
+//   $(modal).addClass('show_modal');
+//   $('body').addClass('fixed').css({ 'top': -scrollPosition1 });
+//   return false;
+// });
+
+
 
 
 
