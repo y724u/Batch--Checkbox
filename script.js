63716ff452f9d5726175b363e6553b66f18121b0
y724u <68385107+y724u@users.noreply.github.com>
2020-11-25 13:01:30 +0900

    js-prev を追記

--- script.js
+++ script.js
@@ -2,20 +2,24 @@ $(function () {
   $('.js-all').on('change', function () {
     const $this =  $(this);
     const target = $this.data('target');
-    // const $checkItem = $('#' + target)
-    // const $checkInput = $($checkItem).find('input');
     const $checkInputs = $('#' + target).find('input');
     $($checkInputs).prop('checked', $this.prop('checked'));
   });
 
   //  js-languageItemのinputが変化（チェック）が入ったら
   $('.js-languageItem input').on('change', function () {
+    const $this =  $(this);
     // その層にあるインプットタグの数を数える
-    const $inputLength = $(this).parent().parent().find('input').length;
+    // const $inputLength = $this.parent().parent().find('input').length;
+    const $inputLength = $this.parents('.js-languageItem').find('input').length;
+    console.log($inputLength);
+
     // その層にチェックされているの数を数える
-    const $checkedLength = $(this).parent().parent().find('li :checked').length;
+    // const $checkedLength = $this.parent().parent().find('li :checked').length;
+    const $checkedLength = $this.parents('.js-languageItem').find('li :checked').length;
     // 親チェックボックス
-    const $parentInput = $(this).parent().parent().prev().find('input');
+    // const $parentInput = $this.parent().parent().prev().find('input');
+    const $parentInput = $this.parents('.js-languageItem').prev('.js-prev').find('input');
     // タグ数とチェックされている数が同じだったら、その親チェックをいれる
     if ($inputLength == $checkedLength) {
       $($parentInput).prop('checked', true);
