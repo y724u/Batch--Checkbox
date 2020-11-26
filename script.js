0d4459c18d9fe181e8bc293be96798904d00e81c
y724u <68385107+y724u@users.noreply.github.com>
2020-11-26 13:04:24 +0900

    js　定数名を修正

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
 $(function () {
   $('.js-all').on('change', function () {
-    const $this =  $(this);
+    const $this = $(this);
     const target = $this.data('target');
     const $checkInputs = $('#' + target).find('input');
     $($checkInputs).prop('checked', $this.prop('checked'));
   });
 
-  //  js-languageItemのinputが変化（チェック）が入ったら
-  $('.js-languageItem input').on('change', function () {
-    const $this =  $(this);
+  //  js-checkBoxItemのinputが変化（チェック）が入ったら
+  $('.js-checkBoxItem input').on('change', function () {
+    const $this = $(this);
+    const $jsAllWrap = $this.parents('.js-checkBoxItem');
+
     // その層にあるインプットタグの数を数える
     // const $inputLength = $this.parent().parent().find('input').length;
-    const $inputLength = $this.parents('.js-languageItem').find('input').length;
-    console.log($inputLength);
+
+    // ペアレンツを使用してインプットの数を数える
+    // const $inputLength = $this.parents('.js-languageItem').find('input').length;
+
+    // inputを全て取得する
+    const $inputItems = $jsAllWrap.find('input');
 
     // その層にチェックされているの数を数える
     // const $checkedLength = $this.parent().parent().find('li :checked').length;
-    const $checkedLength = $this.parents('.js-languageItem').find('li :checked').length;
+
+    // その層にチェックされているの数を数える ペアレンツ
+    // const $checkedLength = $this.parents('.js-languageItem').find('li :checked').length;
+
+    // フィルターを使ってcheckedを取得
+    const $checkedItems = $($inputItems).filter(':checked');
+
     // 親チェックボックス
     // const $parentInput = $this.parent().parent().prev().find('input');
-    const $parentInput = $this.parents('.js-languageItem').prev('.js-prev').find('input');
+
+    const $parentInput = $jsAllWrap.prev('.js-prev').find('input');
     // タグ数とチェックされている数が同じだったら、その親チェックをいれる
-    if ($inputLength == $checkedLength) {
+
+    if ($inputItems.length === $checkedItems.length) {
       $($parentInput).prop('checked', true);
-       // タグ数とチェックされている数が違ったら、その親チェックを外す
+      // タグ数とチェックされている数が違ったら、その親チェックを外す
     } else {
       $($parentInput).prop('checked', false);
     }
